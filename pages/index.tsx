bf685b1c7323d072e4ef18ef0aa38af21d775023
ksim <ksimkati@gmail.com>
2024-02-08 00:59:34 +0100

    refresh index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
-import type { NextPage } from "next";
-import { Inter } from "next/font/google";
-import exp from "constants";
+import BlogCard from "@/components/BlogCard";
+import { InferGetStaticPropsType, NextPage } from "next";
+import { readPostsInfo } from "@/lib/helper";
+import { PostApiResponse } from "@/utils/types";
 
-const inter = Inter({ subsets: ["latin"] });
-
-const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Helló world!</h1>;
+export const getStaticProps = async () => {
+  //   const { readPostsInfo }: postApiResponse = await fetch(
+  //     "http://localhost:3000/api/posts"
+  //   ).then((data) => data.json());
+  const postInfo: PostApiResponse = readPostsInfo();
+  return {
+    props: { posts: postInfo },
+  };
 };
 
-export default Home;
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
+  return (
+    <div className="mxw-3-3x1 mx-auto p-5 space-y-5">
+      {posts.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          desc={post.meta}
+          slug={post.slug}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
